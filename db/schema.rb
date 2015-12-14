637a54d6c52eefd1e9f008ce61def2be7a4f21a1
Katrina <blueyed_qt@hotmail.com>
2015-12-14 14:47:32 -0800

    Added tests for Blog model to blog_spec.  Deleted drop_table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214222621) do
+ActiveRecord::Schema.define(version: 20151214223209) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20151214222621) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "author"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "messages", ["post_id"], name: "index_messages_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.integer  "blog_id"
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires a name' do 
+  	blog = Blog.new(name: "")
+  	blod.valid?
+  	expect(blog.errors[:name].any?).to eq(true)
+  end
+
+  it 'requires a description' do 
+  	blog = Blog.new(description: "")
+  	blog.valid?
+  	expect(blog.errors[:description].any?).to eq(true)
+  end
+
+  it 'has many posts' do 
+  	nlog = Blog.create(name: "Blog", description: "A Blog")
+  	post1 = blog.posts.create(title: "This is a title", content: "Content")
+  	post2 = blog.posts.create(title: "Another title", content: "More content")
+  	expect(blog.posts).to include(post1)
+  	expect(blog.posts).to include(post2)
+  end
+
+  it 'deletes associated posts updon deletion' do 
+  	blog = Blog.create(name: "Blog", description: "A Blog")
+  	blog.posts.create(title: "This is a title", content: "Content")
+  	expect {
+  		blog.destroy
+  	}.to change(Post, :count).by(-1)
+  end
 end
